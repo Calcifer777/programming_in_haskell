5ab44f68723a5b4fed69c9f3b63efe721edde7c8
Marco Filippone <m.filip@hotmail.it>
2019-07-12 10:59:20 +0200

    l10 refactored + l11

---
+++ u02_l11_types.hs
@@ -0,0 +1,21 @@
+halve :: Integer -> Integer
+halve n = n `div` 2
+
+printDouble :: Int -> String
+printDouble n = show (n*2)
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter f (x:xs) = if f x
+                    then x : myFilter f xs
+                    else myFilter f xs
+
+myHead :: [a] -> [a]
+myHead [] = []
+myHead (x:xs) = [x]
+
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> [a]
+myFoldl f init [] = init
+myFoldl f init (x:xs) = myFoldl f newInit xs
+    where newInit = f init x
